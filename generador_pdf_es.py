eb1a64f0c79424c313ddcd8b27f002bdb0b7b43c
Kanade <celestialwarrior90000@gmail.com>
2024-07-25 19:09:22 -0400

    Actualizados los archivos generadores de pdf

--- generador_pdf_es.py
+++ generador_pdf_es.py
@@ -1,11 +1,6 @@
 from fpdf import FPDF
 from datetime import datetime
-
-# Obtiene la fecha y hora actuales
-fecha_actual = datetime.now()
-
-# variable para hacer multipdf la fecha y hora como una cadena (por ejemplo, "2024-07-25_09-30-15")
-nombre_archivo = fecha_actual.strftime("Estudiantes_%Y-%m-%d_%H-%M") + ".pdf"
+from gui import obtener_datos_estudiantes
 
 class PDF(FPDF):
     def header(self):
@@ -29,33 +24,54 @@ class PDF(FPDF):
         # adquirir el numero de pagina
         self.cell(0, 10, 'N° Pagina ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
 
-datos = [ ]*10
 
-# Ejecutar la clase
-pdf = PDF()
-pdf.alias_nb_pages()
 
-#funcion para declarar una pagina
-pdf.add_page()
-pdf.set_font('arial', "",10)
-pdf.cell(w=14,h=15,txt= 'ID',border=1,align='C',fill=0,)
-pdf.cell(w=45,h=15,txt= 'Nombres',border=1,align='C',fill=0,)
-pdf.cell(w=45,h=15,txt= 'Apellidos',border=1,align='C',fill=0,)
-pdf.cell(w=15,h=15,txt= 'Edad',border=1,align='C',fill=0,)
-pdf.cell(w=35,h=15,txt= 'C.I',border=1,align='C',fill=0,)
-pdf.multi_cell(w=0,h=15,txt= 'C.I-Representante',border=1,align='C',fill=0,)
 
-for i,dato in enumerate(datos,1):
-    #datos
-    pdf.cell(w=14,h=8,txt= str(i),border=1,align='C',fill=0,)
-    pdf.cell(w=45,h=8,txt= dato[0],border=1,align='C',fill=0,)
-    pdf.cell(w=45,h=8,txt= (f"{dato[1]} {dato[0]}"),border=1,align='C',fill=0,)
-    pdf.cell(w=15,h=8,txt= str(dato[2]),border=1,align='C',fill=0,)
-    pdf.cell(w=35,h=8,txt= dato[3],border=1,align='C',fill=0,)
-    pdf.multi_cell(w=0,h=8,txt= dato[4],border=1,align='C',fill=0,)
-print("|=================================|")
-print("\nREPORTE EN PDF Generado con Exito\n")
-print("|=================================|")
+def pdf_estudiantes(datos):
+    # Obtiene la fecha y hora actuales
+    fecha_actual = datetime.now()
+    # variable para hacer multipdf la fecha y hora como una cadena (por ejemplo, "2024-07-25_09-30-15")
+    nombre_archivo = fecha_actual.strftime("Estudiantes_%Y-%m-%d_%H-%M") + ".pdf"
 
-#salida de datos, (ruta)
-pdf.output(nombre_archivo, 'F')
+
+    # Ejecutar la clase
+    pdf = PDF()
+    pdf.alias_nb_pages()
+
+    #funcion para declarar una pagina
+    pdf.add_page()
+    pdf.set_font('arial', "",10)
+    pdf.cell(w=14,h=15,txt= 'ID',border=1,align='C',fill=0,)
+    pdf.cell(w=45,h=15,txt= 'Nombres',border=1,align='C',fill=0,)
+    pdf.cell(w=45,h=15,txt= 'Apellidos',border=1,align='C',fill=0,)
+    pdf.cell(w=15,h=15,txt= 'Edad',border=1,align='C',fill=0,)
+    pdf.cell(w=35,h=15,txt= 'C.I',border=1,align='C',fill=0,)
+    pdf.multi_cell(w=0,h=15,txt= 'C.I-Representante',border=1,align='C',fill=0,)
+
+    for dato in datos:
+        #datos
+        pdf.cell(w=14,h=8,txt= str(dato[0]),border=1,align='C',fill=0,)
+        pdf.cell(w=45,h=8,txt= str(f"{dato[1]} {dato[2]}"),border=1,align='C',fill=0,)
+        pdf.cell(w=45,h=8,txt= (f"{dato[3]} {dato[4]}"),border=1,align='C',fill=0,)
+        pdf.cell(w=15,h=8,txt= str(dato[5]),border=1,align='C',fill=0,)
+        pdf.cell(w=35,h=8,txt= str(dato[6]),border=1,align='C',fill=0,)
+        pdf.multi_cell(w=0,h=8,txt= str(dato[7]),border=1,align='C',fill=0,)
+    print("|=================================|")
+    print("\nREPORTE EN PDF Generado con Exito\n")
+    print("|=================================|")
+
+    #salida de datos, (ruta)
+    pdf.output(nombre_archivo, 'F')
+        
+a= obtener_datos_estudiantes() 
+
+pdf_estudiantes(a)
+
+
+
+
+
+
+
+
+
--- generador_pdf_re.py
+++ generador_pdf_re.py
@@ -1,11 +1,6 @@
 from fpdf import FPDF
 from datetime import datetime
-
-# Obtiene la fecha y hora actuales
-fecha_actual = datetime.now()
-
-# variable para hacer multipdf la fecha y hora como una cadena (por ejemplo, "2024-07-25_09-30-15")
-nombre_archivo = fecha_actual.strftime("Representantes_%Y-%m-%d_%H-%M") + ".pdf"
+from gui import obtener_datos_representantes
 
 class PDF(FPDF):
     def header(self):
@@ -29,35 +24,46 @@ class PDF(FPDF):
         # adquirir el numero de pagina
         self.cell(0, 10, 'N° Pagina ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
 
-datos = [ ]*20
+def pdf_representante(datos):
+    # Obtiene la fecha y hora actuales
+    fecha_actual = datetime.now()
 
-# Ejecutar la clase
-pdf = PDF()
-pdf.alias_nb_pages()
+    # variable para hacer multipdf la fecha y hora como una cadena (por ejemplo, "2024-07-25_09-30-15")
+    nombre_archivo = fecha_actual.strftime("Representantes_%Y-%m-%d_%H-%M") + ".pdf"
 
-#funcion para declarar una pagina
-pdf.add_page()
-#estilo texto
-pdf.set_font('arial', "",10)
-#celdas
-pdf.cell(w=12,h=15,txt= '#',border=1,align='C',fill=0,)
-pdf.cell(w=25,h=15,txt= 'C.I',border=1,align='C',fill=0,)
-pdf.cell(w=37,h=15,txt= 'Nombres',border=1,align='C',fill=0,)
-pdf.cell(w=37,h=15,txt= 'Apellidos',border=1,align='C',fill=0,)
-pdf.cell(w=30,h=15,txt= 'Telefono',border=1,align='C',fill=0,)
-pdf.multi_cell(w=0,h=15,txt= 'Dirección',border=1,align='C',fill=0,)
+    # Ejecutar la clase
+    pdf = PDF()
+    pdf.alias_nb_pages()
 
-for i,dato in enumerate(datos,1):
-    #datos
-    pdf.cell(w=12,h=8,txt= str(i),border=1,align='C',fill=0,)
-    pdf.cell(w=25,h=8,txt= str(dato[0]),border=1,align='C',fill=0,)
-    pdf.cell(w=37,h=8,txt= (f"{dato[1]} {dato[2]}"),border=1,align='C',fill=0,)
-    pdf.cell(w=37,h=8,txt= (f"{dato[2]} {dato[1]}"),border=1,align='C',fill=0,)
-    pdf.cell(w=30,h=8,txt= dato[3],border=1,align='C',fill=0,)
-    pdf.multi_cell(w=0,h=8,txt= dato[4],border=1,align='C',fill=0,)
-print("|=================================|")
-print("\nREPORTE EN PDF Generado con Exito\n")
-print("|=================================|")
-print(f"el archivo tiene el nombre de:{nombre_archivo} ")
-#salida de datos, (ruta)
-pdf.output(nombre_archivo, 'F')
+    #funcion para declarar una pagina
+    pdf.add_page()
+    #estilo texto
+    pdf.set_font('arial', "",10)
+    #celdas
+    pdf.cell(w=12,h=15,txt= '#',border=1,align='C',fill=0,)
+    pdf.cell(w=25,h=15,txt= 'C.I',border=1,align='C',fill=0,)
+    pdf.cell(w=37,h=15,txt= 'Nombres',border=1,align='C',fill=0,)
+    pdf.cell(w=37,h=15,txt= 'Apellidos',border=1,align='C',fill=0,)
+    pdf.cell(w=30,h=15,txt= 'Telefono',border=1,align='C',fill=0,)
+    pdf.multi_cell(w=0,h=15,txt= 'Dirección',border=1,align='C',fill=0,)
+
+    for i,dato in enumerate(datos,1):
+        #datos
+        pdf.cell(w=12,h=8,txt= str(i),border=1,align='C',fill=0,)
+        pdf.cell(w=25,h=8,txt= str(dato[0]),border=1,align='C',fill=0,)
+        pdf.cell(w=37,h=8,txt= (f"{dato[1]} {dato[2]}"),border=1,align='C',fill=0,)
+        pdf.cell(w=37,h=8,txt= (f"{dato[2]} {dato[1]}"),border=1,align='C',fill=0,)
+        pdf.cell(w=30,h=8,txt= dato[3],border=1,align='C',fill=0,)
+        pdf.multi_cell(w=0,h=8,txt= dato[4],border=1,align='C',fill=0,)
+    print("|=================================|")
+    print("\nREPORTE EN PDF Generado con Exito\n")
+    print("|=================================|")
+    print(f"el archivo tiene el nombre de:{nombre_archivo} ")
+    #salida de datos, (ruta)
+    pdf.output(nombre_archivo, 'F')
+
+a= obtener_datos_representantes()
+pdf_representante(a)
+
+
+
